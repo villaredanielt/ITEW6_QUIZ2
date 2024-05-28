fff10901b82cb5b8b2108b5db41c3d3b2055f9e3
Sheena Jane Toroy <91197998+sheenatoroy@users.noreply.github.com>
2024-05-29 02:03:52 +0800

    re-creating the frontend due to some conflicts and issues encountered

--- vue-frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';  // Ensure this path is correct
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
